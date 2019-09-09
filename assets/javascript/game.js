cbb8387f080224ea0128de10bbe7be3ce9340151
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-09 13:58:24 -0700

    JavaScript - random number at the beginning of the game is set between 120 and 19. Crystals have hidden number between 1 and 12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,25 @@
-let startNum = Math.floor(Math.random() * 120) + 19
+let targetNum = Math.floor(Math.random() * 120) + 19
 let current = 0
 let wins = 0
 let losses = 0
 
+const renderGems = () => {
+  current = 0
+  targetNum =  Math.floor(Math.random() * 120) + 19
+  document.getElementById(`crystals`).innerHTML = ` `
+  for (let i = 0; i <= 4; i ++){
+    const random = Math.floor(Math.random () * 12) + 1
+    let gemElem = document.createElement(`div`)
+    gemElem.className = `col-3`
+    gemElem.innerHTML = `
+      <div class="card" >
+            <img class="crystal" src="./gem${i + 1}.png" data-value="${random}">
+        </div>
+    `
+    document.getElementById(`crystals`).append(gemElem)
+  }
+  document.getElementById('targetNum').textContent = targetNum
+  document.getElementById('current').textContent = current
+  document.getElementById('wins').textContent = wins
+  document.getElementById('losses').textContent = losses
+}
