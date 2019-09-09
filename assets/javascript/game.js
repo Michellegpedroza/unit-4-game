ad3a9077c735c64872331214a67147556f980581
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-09 14:05:00 -0700

    adding an event listener to the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,16 +12,21 @@ const renderGems = () => {
     let gemElem = document.createElement(`div`)
     gemElem.className = `col-3`
     gemElem.innerHTML = `
-      <div class="card" >
+      <div class="card border-0 bg-light pb-2" >
             <img class="crystal" src="./assets/images/gem${i + 1}.png" data-value="${random}" width="200" height="200">
         </div>
     `
     document.getElementById(`crystals`).append(gemElem)
   }
   document.getElementById('targetNum').textContent = targetNum
-  document.getElementById('current').textContent = current
+  document.getElementById('currentNum').textContent = currentNum
   document.getElementById('wins').textContent = wins
   document.getElementById('losses').textContent = losses
 }
 
+document.addEventListener('click', event => {
+  if (event.target.className === 'crystal' ) {
+    
+  }
+})
 renderGems()
