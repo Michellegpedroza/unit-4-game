ad00dba465fac4bbc708eaf73222c3af4a6e02fc
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-09 15:10:26 -0700

    added an icon on the pages tab

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,16 @@
+// Starting Variables
 let targetNum = Math.floor(Math.random() * 100) + 19
 let currentNum = 0
 let wins = 0
 let losses = 0
 
+//render crystals function
 const renderCrystals = () => {
+  //reset game
   currentNum = 0
+  //sets the new number play needs to reach
   targetNum = Math.floor(Math.random() * 100) + 19
+  //render crystals
   document.getElementById(`crystals`).innerHTML = ` `
   for (let i = 0; i < 4; i++) {
     const random = Math.floor(Math.random() * 12) + 1
@@ -18,21 +23,25 @@ const renderCrystals = () => {
     `
     document.getElementById(`crystals`).append(crystalElem)
   }
+  //results 
   document.getElementById('targetNum').textContent = targetNum
   document.getElementById('currentNum').textContent = currentNum
   document.getElementById('wins').textContent = wins
   document.getElementById('losses').textContent = losses
 }
 
+//click events for the crystal images
 document.addEventListener('click', event => {
   if (event.target.className === 'crystal') {
     let crystalValue = parseInt(event.target.dataset.value)
     currentNum += crystalValue
     if (currentNum === targetNum) {
       wins++
+      alert(`You won! You reached the number ${targetNum}`)
       renderCrystals()
     } else if (currentNum > targetNum) {
       losses++
+      alert(`The target number was ${targetNum} try again!`)
       renderCrystals()
     } else {
       document.getElementById(`currentNum`).textContent = currentNum
